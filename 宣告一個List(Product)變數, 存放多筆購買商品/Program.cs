688548c26ed2cf58bfc423dddf63e163be9c931b
建均 陳 <protosscarrier@gmail.com>
2022-10-22 17:03:56 +0800

    加入專案檔案。

---
+++ 宣告一個List(Product)變數, 存放多筆購買商品/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 宣告一個List_Product_變數__存放多筆購買商品
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<string, int> product = new Dictionary<string, int>()
+			{
+				{"apple",6 },
+				{"banana",10},
+				{"bread" ,2},
+			};
+
+			
+		}
+	}
+}
